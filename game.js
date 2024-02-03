1253380ff9fe377172b30565d434881e809fe0e6
Piyush M Raut <106990915+piyushmraut@users.noreply.github.com>
2024-02-04 00:40:57 +0530

    game.js

--- game.js
+++ game.js
@@ -1,38 +1,3 @@
-// let gameseq = [];
-// let userseq = [];
-
-// let level = 0;
-// let started = false;
-
-
-
-// let h2 = document.querySelector("h2");
-
-// document.addEventListener("keypress",function(){
-//     if(started==false)
-//     {
-//         console.log("Game Started");
-//         started = true;
-//         levelUp();
-//     }
-    
-//  });
-
-//  function levelUp(){
-//     level++;
-//     h2.innerText = `Level ${level}`;
-
-//     btnflash();
-//  }
-
-
-//  function btnflash(){
-//     btnflash.classList.add("flash");
-
-//     setTimeout(function(){
-//         btn.classList.remove("flash");
-//     },1000);
-//  }
 
 let gameSeq = [];
 let userSeq = [];
